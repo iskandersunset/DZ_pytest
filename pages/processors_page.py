70d857f0b95e85db5c5e460a5df360c947c95769
Александр Халезин <iskandersunset@gmail.com>
2022-10-21 18:24:43 +0500

    Завис на смещении ползунка цены

---
+++ pages/processors_page.py
@@ -0,0 +1,33 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from base.base_class import Base
+
+
+class Processors_page(Base):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    # Locators============================
+    price_slider = ".css-zuxqkm"
+    price_right = ""
+
+    # Getters=============================
+
+    def get_price_slider(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.price_slider)))
+
+    # Actions =============================
+    def click_price_slider(self):
+        move = ActionChains(self.driver)
+        move.click_and_hold(self.get_price_slider()).move_by_offset(20, 0).release().perform()
+        print("Ползунок тащим левый")
+
+    # Methods =============================
+    def price_set(self):
+        self.click_price_slider()
+    #     self.click_select_product_1()
