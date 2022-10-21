06847cb8420fec31d8780236cc90eb7ec16a0263
Александр Халезин <iskandersunset@gmail.com>
2022-10-21 15:48:06 +0500

    Добавили отмену регистрации, выбор геолокации

---
+++ tests/test_by_product.py
@@ -0,0 +1,21 @@
+import time
+
+# import pytest as pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from pages.login_page import Login_page
+
+
+# @pytest.mark.run(order=1)
+def test_by_product_1(set_group):
+    s = Service('C:/_teach/resource/chromedriver.exe')
+    driver = webdriver.Chrome(service=s)
+
+    print('START TEST 1')
+
+    login = Login_page(driver)
+    login.authorization()
+
+    print('FINISH TEST 1')
+    time.sleep(10)
+
