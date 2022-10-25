cddd48b3d30de5b4ec2b9a463683f76ce0451b7c
Александр Халезин <iskandersunset@gmail.com>
2022-10-25 10:54:51 +0500

    Добавил try конструкции для большей стабильности

--- pages/processorspage.py
+++ pages/processorspage.py
@@ -1,9 +1,10 @@
 import time
 
+from selenium.common import ElementClickInterceptedException, StaleElementReferenceException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 
 from base.base_class import Base
 
@@ -20,6 +21,7 @@ class ProcessorsPage(Base):
     price_slider_right = "/html/body/div[3]/div[2]/main/section/div[1]/div[2]/div/div/div[2]" \
                          "/div[2]/div/div[3]/div[2]/div[3]/div/div[5]"
     proc_brand = "//input[@id='intel']"
+    proc_brand_check = "data-meta-is-selected"
     proc_num_core = "//input[@id='8554_2612']"
     sort_by_price = "//div[@data-alias='price']"
     product = "ProductCardHorizontal__title"  # Наименование товара
@@ -30,19 +32,19 @@ class ProcessorsPage(Base):
     # Getters=============================
 
     def get_price_slider_left(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.price_slider_left)))
+        return WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.XPATH, self.price_slider_left)))
 
     def get_price_slider_right(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.price_slider_right)))
+        return WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.XPATH, self.price_slider_right)))
 
     def get_proc_brand(self):
-        return WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.XPATH, self.proc_brand)))
+        return WebDriverWait(self.driver, 30).until(ec.presence_of_element_located((By.XPATH, self.proc_brand)))
 
     def get_proc_num_core(self):
-        return WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.XPATH, self.proc_num_core)))
+        return WebDriverWait(self.driver, 30).until(ec.presence_of_element_located((By.XPATH, self.proc_num_core)))
 
     def get_sort_by_price(self):
-        return WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.XPATH, self.sort_by_price)))
+        return WebDriverWait(self.driver, 30).until(ec.presence_of_element_located((By.XPATH, self.sort_by_price)))
 
     '''Выбираем самый дорогой товар, список отсортирован по убыванию цены.'''
     def get_product(self):
@@ -61,45 +63,75 @@ class ProcessorsPage(Base):
     def move_price_slider_left(self):
         move = ActionChains(self.driver)
         move.click_and_hold(self.get_price_slider_left()).move_by_offset(50, 0).release().perform()
+        time.sleep(3)
         print(" === Move left slide === ")
 
     def move_price_slider_right(self):
-        move = ActionChains(self.driver)
-        move.click_and_hold(self.get_price_slider_right()).move_by_offset(-20, 0).release().perform()
-        print(" === Move right slider === ")
+        try:
+            move = ActionChains(self.driver)
+            move.click_and_hold(self.get_price_slider_right()).move_by_offset(-20, 0).release().perform()
+            print(" === Move right slider === ")
+        except ElementClickInterceptedException:
+            move = ActionChains(self.driver)
+            move.click_and_hold(self.get_price_slider_right()).move_by_offset(-20, 0).release().perform()
+            print('ElementClickInterceptedException')
+            print(" === Move right slider === ")
 
     def checkbox_proc_brand(self):
-        self.driver.execute_script("arguments[0].scrollIntoView(); window.scrollBy(0, -window.innerHeight / 4);",
-                                   self.get_proc_brand())
-        self.get_proc_brand().click()
-        print(" === Choose a brand === ")
+        try:
+            self.driver.execute_script("arguments[0].scrollIntoView(); window.scrollBy(0, -window.innerHeight "
+                                       "/ 4);", self.get_proc_brand())
+            self.get_proc_brand().click()
+            print(" === Choose a brand === ")
+        except ElementClickInterceptedException:
+            self.driver.refresh()
+            self.driver.execute_script("arguments[0].scrollIntoView(); window.scrollBy(0, -window.innerHeight "
+                                       "/ 4);", self.get_proc_brand())
+            self.get_proc_brand().click()
+            print(" === ElementClickInterceptedException === ")
+            print(" === Choose a brand === ")
 
     def checkbox_proc_num_core(self):
-        self.driver.execute_script("arguments[0].scrollIntoView(); window.scrollBy(0, -window.innerHeight / 4);",
-                                   self.get_proc_num_core())
-        self.get_proc_num_core().click()
-        print(" === Choose the number of cores === ")
+        try:
+            self.driver.execute_script("arguments[0].scrollIntoView(); window.scrollBy(0, -window.innerHeight "
+                                       "/ 4);", self.get_proc_num_core())
+            self.get_proc_num_core().click()
+            print(" === Choose the number of cores === ")
+        except ElementClickInterceptedException:
+            self.driver.refresh()
+            self.driver.execute_script("arguments[0].scrollIntoView(); window.scrollBy(0, -window.innerHeight "
+                                       "/ 4);", self.get_proc_num_core())
+            self.get_proc_num_core().click()
+            print(" === ElementClickInterceptedException === ")
+            print(" === Choose the number of cores === ")
 
     def click_sort_by_price(self):
-        self.driver.execute_script("window.scrollTo(0, -document.body.scrollTop);")
-        self.get_sort_by_price().click()
-        self.get_sort_by_price().click()
-        print(" === Click sort by price === ")
+        try:
+            self.driver.execute_script("window.scrollTo(0, -document.body.scrollTop);")
+            self.get_sort_by_price().click()
+            self.get_sort_by_price().click()
+            print(" === Click sort by price === ")
+        except StaleElementReferenceException:
+            self.driver.execute_script("window.scrollTo(0, -document.body.scrollTop);")
+            self.get_sort_by_price().click()
+            self.get_sort_by_price().click()
+            print(" === StaleElementReferenceException === ")
+            print(" === Click sort by price === ")
 
     def click_get_button_add_cart(self):
-        self.get_button_add_cart().click()
-        print(" === Click Add to Cart === ")
+        try:
+            self.get_button_add_cart().click()
+            print(" === Click Add to Cart === ")
+        except StaleElementReferenceException:
+            self.get_button_add_cart().click()
+            print(" === StaleElementReferenceException === ")
+            print(" === Click Add to Cart === ")
 
     # Methods =============================
     def price_set(self):
         self.move_price_slider_left()
-        time.sleep(3)
         self.move_price_slider_right()
-        time.sleep(3)
         self.checkbox_proc_brand()
-        time.sleep(3)
         self.checkbox_proc_num_core()
-        time.sleep(3)
         self.click_sort_by_price()
-        time.sleep(2)
         self.click_get_button_add_cart()
