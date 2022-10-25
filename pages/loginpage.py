358cc96bcbbd99d0ef2828a8d92b29a2cd5237f8
Александр Халезин <iskandersunset@gmail.com>
2022-10-25 10:54:23 +0500

    EC а ec поменял

--- pages/loginpage.py
+++ pages/loginpage.py
@@ -1,6 +1,7 @@
+from selenium.common import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 
 from base.base_class import Base
 
@@ -22,21 +23,25 @@ class LoginPage(Base):
     # Getters=============================
 
     def get_close_button(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, self.close_button)))
+        return WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.CLASS_NAME, self.close_button)))
 
     def get_geo_menu(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, self.geo_menu)))
+        return WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.CLASS_NAME, self.geo_menu)))
 
     def get_citi_select(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.citi_select)))
+        return WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.XPATH, self.citi_select)))
 
     def get_catalog_proc(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.catalog_proc)))
+        return WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.XPATH, self.catalog_proc)))
 
     # Actions =============================
     def click_close_button(self):
-        self.get_close_button().click()
-        print(" === Click close registration === " * 10)
+        try:
+            self.get_close_button().click()
+            print(" === Click close registration === " * 10)
+        except ElementClickInterceptedException:
+            self.get_close_button().click()
+            print(" === Click close registration === " * 10)
 
     def click_geo_menu(self):
         self.get_geo_menu().click()
@@ -64,4 +69,3 @@ class LoginPage(Base):
         self.assert_url('https://www.citilink.ru/catalog/processory/')
         self.get_screenshot()
         # self.assert_word(self.get_main_word(), 'PRODUCTS')
-
