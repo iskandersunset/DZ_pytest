0daae140671ce190e1d00696e7d8e7d38ed2dcc8
Александр Халезин <iskandersunset@gmail.com>
2022-10-27 09:49:17 +0500

    Ассерты

--- tests/test_by_product.py
+++ tests/test_by_product.py
@@ -1,13 +1,4 @@
-import time
-
-import pytest
-
 from pages.cartpage import CartPage
-# import pytest as pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium_stealth import stealth
-
 from pages.loginpage import LoginPage
 from pages.processorspage import CategoryPage
 
@@ -15,6 +6,9 @@ from pages.processorspage import CategoryPage
 def test_by_processor(browser, set_up):
 
     lp = LoginPage(browser)
+    citi = lp.get_geo_menu().text
+    print('citi')
+    assert citi == 'Челябинск'
     lp.choose_category()
 
     cgp = CategoryPage(browser)
