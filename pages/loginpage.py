35bb45e68c14bfdeaa5afe6b359e9c3c0b5af690
Александр Халезин <iskandersunset@gmail.com>
2022-10-27 09:51:08 +0500

    Remove unused imports

--- pages/loginpage.py
+++ pages/loginpage.py
@@ -1,7 +1,5 @@
 from selenium.common import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 
 from base.baseapp import BasePage
 
