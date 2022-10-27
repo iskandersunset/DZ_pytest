6d48d520f23bd555a4ba4e5c9f2f6d4762d4f4d7
Александр Халезин <iskandersunset@gmail.com>
2022-10-27 09:50:51 +0500

    Remove unused imports

--- pages/processorspage.py
+++ pages/processorspage.py
@@ -4,8 +4,6 @@ from selenium import webdriver
 from selenium.common import ElementClickInterceptedException, StaleElementReferenceException
 from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 
 from base.baseapp import BasePage
 
