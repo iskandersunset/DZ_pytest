5f5ed211ca19671c4fe3833031da988a42274588
Александр Халезин Дома <iskandersunset@gmail.com>
2022-10-29 18:30:02 +0500

    Смена явного на неявное

--- pages/loginpage.py
+++ pages/loginpage.py
@@ -5,31 +5,29 @@ from base.baseapp import BasePage
 
 
 class LoginPage(BasePage):
+    # Locators============================
+    close_button = (By.CLASS_NAME, "Tooltip__close-mini")  # Закрыть пределожение регистрации
+    geo_menu = (By.CLASS_NAME, "MainHeader__city")  # Выбираем меню геолокации
+    citi_select = (By.XPATH, "//a[@data-search='челябинск']")
+    catalog_proc = (By.XPATH, "//a[@href='catalog/processory/']")
 
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
 
-    # Locators============================
-
-    close_button = (By.CLASS_NAME, "Tooltip__close-mini")  # Закрыть прделожение регистрации
-    geo_menu = (By.CLASS_NAME, "MainHeader__city")  # Выбираем меню геолокации
-    citi_select = (By.XPATH, "//a[@data-search='челябинск']")
-    catalog_proc = (By.XPATH, "//a[@href='catalog/processory/']")
-
-    # Getters=============================
+    # Getters =============================
 
     def get_close_button(self):
-        return self.find_element(self.close_button)
+        return self.driver.find_element(*self.close_button)
 
     def get_geo_menu(self):
-        return self.find_element(self.geo_menu)
+        return self.driver.find_element(*self.geo_menu)
 
     def get_citi_select(self):
-        return self.find_element(self.citi_select)
+        return self.driver.find_element(*self.citi_select)
 
     def get_catalog_proc(self):
-        return self.find_element(self.catalog_proc)
+        return self.driver.find_element(*self.catalog_proc)
 
     # Actions =============================
     def click_close_button(self):
@@ -44,7 +42,7 @@ class LoginPage(BasePage):
         self.get_geo_menu().click()
         print(" === Click Menu Geolocation === ")
 
-    def click_citi_select(self):
+    def click_city_select(self):
         self.get_citi_select().click()
         print(" === Click Citi Chelyabinsk === ")
 
@@ -56,9 +54,12 @@ class LoginPage(BasePage):
     def choose_category(self):
         self.click_close_button()
         self.click_geo_menu()
-        self.click_citi_select()
+        self.click_city_select()
+
         self.assert_word(self.get_geo_menu(), 'Челябинск')
+
         self.get_screenshot()
+
         self.click_catalog_proc()
         self.assert_url('https://www.citilink.ru/catalog/processory/')
         self.get_screenshot()
