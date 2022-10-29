4618aee4a7e694406183e1ffcde1754f85d6c283
Александр Халезин Дома <iskandersunset@gmail.com>
2022-10-29 18:27:34 +0500

    Красявости

--- base/baseapp.py
+++ base/baseapp.py
@@ -3,21 +3,30 @@ import time
 
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import Chrome
 
 
 class BasePage:
 
-    def __init__(self, driver):
+    def __init__(self, driver: Chrome):
         self.driver = driver
         self.base_url = 'https://www.citilink.ru/'
 
-    def find_elements(self, locator, time=20):
-        return WebDriverWait(self.driver, time).until(EC.presence_of_all_elements_located(locator),
-                                                      message=f"Can't find elements by locator {locator}")
+        driver.implicitly_wait(10)
 
-    def find_element(self, locator, time=20):
-        return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
-                                                      message=f"Can't find element by locator {locator}")
+    # def find_elements(self, locator, time=30):
+    #     return WebDriverWait(self.driver, time).until(EC.presence_of_all_elements_located(locator),
+    #                                                   message=f"Can't find elements by locator {locator}")
+    #
+    # def find_element(self, locator, time=30):
+    #     return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
+    #                                                   message=f"Can't find element by locator {locator}")
+
+
+    def should_be_on_page(self, locator):
+        element = self.driver.find_element(locator)
+        assert element, f"Element {locator} does not found."
+        return element
 
     """Method Get current URL"""
 
@@ -43,8 +52,8 @@ class BasePage:
     """Method Assert word"""
 
     def assert_url(self, result):
-        get_url = self.driver.current_url
-        assert get_url == result
+        url = self.driver.current_url
+        assert url == result, f"Wrong url: {url}"
         print(" === Good assert URL === ")
 
     def assert_selected(self, checkbox):
